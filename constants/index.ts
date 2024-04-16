170cddf4619a1313893197d307c21597c9e447a9
RalphFred <raphaelgbaorun@gmail.com>
2024-04-16 16:03:33 +0100

    build: did what i did

---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const heroImages = [
+  {
+    imgUrl: '/images/img1.jpg',
+    description: 'Hero Image'
+  },
+  {
+    imgUrl: '/images/img2.jpg',
+    description: 'Hero Image'
+  },
+  {
+    imgUrl: '/images/img3.jpg',
+    description: 'Hero Image'
+  },
+]
